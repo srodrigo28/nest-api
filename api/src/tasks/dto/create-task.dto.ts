8ba2a860ffc7842c07dd2d6c1a987d676fa33ab5
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2025-12-21 19:42:08 -0300

    validação com validators

--- api/src/tasks/dto/create-task.dto.ts
+++ api/src/tasks/dto/create-task.dto.ts
@@ -4,7 +4,17 @@
  *  Validar dados de entrada transformando-os em um formato adequado para o sistema.
  */
 
+import { IsNotEmpty, IsString, MinLength } from "class-validator";
+
 export class CreateTaskDto {
+
+    @IsString( {message: 'O nome deve ser uma string' } )
+    @IsNotEmpty({ message: 'O nome não pode estar vazio' })
+    @MinLength(5, { message: 'O nome deve ter no mínimo 5 caracteres' })
     readonly name: string;
+
+    @IsString( {message: 'A descrição deve ser uma string' } )
+    @IsNotEmpty( { message: 'A descrição não pode estar vazia' } )
+    @MinLength(10, { message: 'A descrição deve ter no mínimo 10 caracteres' })
     readonly description: string;
 }
--- api/src/tasks/dto/update-task.dto.ts
+++ api/src/tasks/dto/update-task.dto.ts
@@ -1,5 +1,17 @@
+import { IsOptional, IsString, MinLength } from "class-validator";
+
 export class UpdateTaskDto {
+
+    @IsOptional()
+    @IsString( {message: 'O nome deve ser uma string' } )
+    @MinLength(5, { message: 'O nome deve ter no mínimo 5 caracteres' })    
     readonly name?: string;
+
+    @IsOptional()
+    @IsString( {message: 'A descrição deve ser uma string' } )
+    @MinLength(10, { message: 'A descrição deve ter no mínimo 10 caracteres' }) 
     readonly description?: string;
+
+    @IsOptional()
     readonly completed?: boolean;
 }
